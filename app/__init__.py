86dbeb7e57004950dc06515090c92b18b1b9dcdd
adrienycart <adrien.ycart@gmail.com>
2019-09-24 21:18:18 +0100

    nice formatting for last_seen

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from logging.handlers import RotatingFileHandler
+
 import os
 
 
@@ -19,6 +21,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 app.jinja_env.filters['zip'] = zip
 
