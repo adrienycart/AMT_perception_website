338afd50e6059790a70312668029f7171a2c05ec
adrienycart <adrien.ycart@gmail.com>
2019-12-04 19:27:51 +0000

    give more time to answer

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 MAX_ANSWERS = 4
 DATA_PATH = 'data/all_mp3_cut'
 MIN_DATE = datetime.datetime(datetime.MINYEAR,1,1,0,0)
-LOCK_TIME = datetime.timedelta(minutes=2) #Amount of timeor which other users cannot access the same question.
+LOCK_TIME = datetime.timedelta(minutes=5) #Amount of timeor which other users cannot access the same question.
 
 
 class Config(object):
