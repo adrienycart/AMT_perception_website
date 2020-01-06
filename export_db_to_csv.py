f5e735f6054b1a8d99f82a2650d1246d3bd0c785
adrienycart <adrien.ycart@gmail.com>
2020-01-06 20:45:40 +0000

    Update export_db_to_csv.py

--- export_db_to_csv.py
+++ export_db_to_csv.py
@@ -35,7 +35,7 @@ for user in User.query.all():
         user_data += [parse_answers(user)]
 
 user_data = np.array(user_data,dtype=object)
-np.savetxt("user_data.csv",user_data, fmt="%s")
+np.savetxt("db_csv/user_data.csv",user_data, fmt="%s")
 
 
 # ANSWERS DATA
@@ -53,7 +53,7 @@ for question in Question.query.filter(Question.n_answers==MAX_ANSWERS).all():
 
 answers_data = np.array(answers_data,dtype=object)
 # print answers_data
-np.savetxt("answers_data.csv",answers_data, fmt="%s")
+np.savetxt("db_csv/answers_data.csv",answers_data, fmt="%s")
 
 # COMMENTS
 comments = ""
@@ -65,6 +65,6 @@ for user in User.query.all():
         comments += user.comments
         comments += "\n"
 
-text_file = open("comments.txt", "w")
+text_file = open("db_csv/comments.txt", "w")
 n = text_file.write(comments)
 text_file.close()
