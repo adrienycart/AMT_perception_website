3e0c23c94e7d4e05ab51612c781422c74368dde9
cherie <fj123420005@126.com>
2020-01-09 15:07:42 +0000

    rhythm histogram to logged value & add rhythm dispersion (centre drift only, no std)

--- features/rhythm.py
+++ features/rhythm.py
@@ -2,34 +2,91 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-SMALL_VALUE = 0.5
+SMALL_VALUE = 0.0001
 
 def rhythm_histogram(intervals_output, intervals_target):
-	# return the spectral flatness ratio of the IOIs
-	# 1. spectral flatness for IOI of the output transcription
-	# 2. spectral flatness for IOI of the ground truth music piece
-	ioi_output = [intervals_output[idx][1] - intervals_output[idx][0] for idx in range(len(intervals_output))]
-	ioi_target = [intervals_target[idx][1] - intervals_target[idx][0] for idx in range(len(intervals_target))]
+    # return the logged spectral flatness ratio of the IOIs
+    # 1. spectral flatness for IOI of the output transcription
+    # 2. spectral flatness for IOI of the ground truth music piece
+    ioi_output = [float(intervals_output[idx][1] - intervals_output[idx][0]) for idx in range(len(intervals_output))]
+    ioi_target = [float(intervals_target[idx][1] - intervals_target[idx][0]) for idx in range(len(intervals_target))]
 
-	# generate bins
-	bins = [i*0.01 for i in range(10)]
-	bins += [0.1+i*0.1 for i in range(20)]
+    # generate bins
+    bins = [i*0.01 for i in range(10)]
+    bins += [0.1+i*0.1 for i in range(20)]
 
-	histogram_output = np.histogram(ioi_output, bins=bins)[0]
-	histogram_target = np.histogram(ioi_target, bins=bins)[0]
+    histogram_output = np.histogram(ioi_output, bins=bins)[0]
+    histogram_target = np.histogram(ioi_target, bins=bins)[0]
 
-	for i in range(len(bins)-1):
-		if histogram_output[i] == 0:
-			histogram_output[i] = SMALL_VALUE
-		if histogram_target[i] == 0:
-			histogram_target[i] = SMALL_VALUE
+    histogram_output = [max(SMALL_VALUE, histogram_output[idx]) for idx in range(len(histogram_output))]
+    histogram_target = [max(SMALL_VALUE, histogram_target[idx]) for idx in range(len(histogram_target))]
 
-	gmean_output = stats.mstats.gmean(histogram_output)
-	gmean_target = stats.mstats.gmean(histogram_target)
-	mean_output = np.mean(histogram_output)
-	mean_target = np.mean(histogram_target)
-	print(gmean_target)
+    log_gmean_output = np.mean(np.log(histogram_output))
+    log_gmean_target = np.mean(np.log(histogram_target))
+    mean_output = np.mean(histogram_output)
+    mean_target = np.mean(histogram_target)
 
-	return gmean_output / mean_output, gmean_target / mean_target
+    return log_gmean_output - np.log(mean_output), log_gmean_target - np.log(mean_target)
 
 
+def rhythm_dispersion(intervals_output, intervals_target):
+    # return changes in k-means clusters
+    # 1. change in standard deviations (k-means doesn't have std definition...)
+    # 2. center drift (average drift and max drift)
+
+    ioi_output = [float(intervals_output[idx][1] - intervals_output[idx][0]) for idx in range(len(intervals_output))]
+    ioi_target = [float(intervals_target[idx][1] - intervals_target[idx][0]) for idx in range(len(intervals_target))]
+
+    # initialise cluster
+    bins = [i*0.01 for i in range(10)]
+    bins += [0.1+i*0.1 for i in range(20)]
+    histogram_target = np.histogram(ioi_target, bins=bins)[0]
+    means = []
+    for i in range(1, len(histogram_target)-1):
+        if histogram_target[i] > 0 and histogram_target[i] >= histogram_target[i-1] and histogram_target[i] >= histogram_target[i+1]:
+            means.append(np.mean([bins[i], bins[i+1]]))
+            
+    # k-means on target intervals
+    moving = 1
+    while moving > 0.0001:
+        # cluster
+        ioi_target_labels = [np.argmin(abs(np.array(means) - ioi_target[idx])) for idx in range(len(ioi_target))]
+        # calculate new means
+        new_means = []
+        for label in range(len(means)):
+            indexs = np.array([i for i in range(len(ioi_target)) if ioi_target_labels[i] == label])
+            if len(indexs) > 0:
+                new_means.append(np.mean(np.array(ioi_target)[indexs]))
+            else:
+                new_means.append(means[label])
+        # calculate centre moving
+        moving = np.sum(abs(np.array(new_means) - np.array(means)))
+        # update means
+        means = new_means
+        # print(means)
+
+    # print('--')
+    # k-means on output intervals
+    means_output = [means[i] for i in range(len(means))] # copy target means
+    moving = 1
+    while moving > 0.0001:
+        # cluster
+        ioi_output_labels = [np.argmin(abs(np.array(means_output) - ioi_output[idx])) for idx in range(len(ioi_output))]
+        # calculate new means
+        new_means_output = []
+        for label in range(len(means_output)):
+            indexs = np.array([i for i in range(len(ioi_output)) if ioi_output_labels[i] == label])
+            if len(indexs) > 0:
+                new_means_output.append(np.mean(np.array(ioi_output)[indexs]))
+            else:
+                new_means_output.append(means_output[label])
+        # calculate centre moving
+        moving = np.sum(abs(np.array(new_means_output) - np.array(means_output)))
+        # update means
+        means_output = new_means_output
+        # print(means_output)
+
+    # cluster centre drift
+    drifts = [abs(means[idx] - means_output[idx]) for idx in range(len(means))]
+
+    return np.mean(drifts), max(drifts)
--- test_features.py
+++ test_features.py
@@ -10,7 +10,7 @@ from features.out_key import make_key_mask, out_key_errors, out_key_errors_binar
 from features.polyphony import polyphony_level_seq, false_negative_polyphony_level
 from features.repeat_merge import repeated_notes, merged_notes
 from features.specific_pitch import specific_pitch_framewise, specific_pitch_notewise
-from features.rhythm import rhythm_histogram
+from features.rhythm import rhythm_histogram, rhythm_dispersion
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -87,7 +87,7 @@ systems = ['kelz', 'lisu', 'google', 'cheng']
 fs = 44100
 
 
-for example in os.listdir(MIDI_path)[:10]:
+for example in os.listdir(MIDI_path)[:20]:
 	example_path = os.path.join(MIDI_path, example)  # folder path
 	print('\n\npath = ' + example_path)
 	target_data = pm.PrettyMIDI(os.path.join(example_path, 'target.mid'))
@@ -115,46 +115,48 @@ for example in os.listdir(MIDI_path)[:10]:
 			match_onoff = mir_eval.transcription.match_notes(intervals_target, notes_target, intervals_system, notes_system, offset_ratio=0.2, pitch_tolerance=0.25)
 
 			# test features...
-			print('\n test high_low_voice =======================================')
-			print('\n>>>> test high_low_voice framewise')
-			test_high_low_voice_framewise(system_pr, target_pr)
+			# print('\n test high_low_voice =======================================')
+			# print('\n>>>> test high_low_voice framewise')
+			# test_high_low_voice_framewise(system_pr, target_pr)
 
-			print('\n>>>> test high_low_voice notewise, onset only')
-			test_high_low_voice_notewise(notes_system, intervals_system, notes_target, intervals_target, match_on)  # onset only
+			# print('\n>>>> test high_low_voice notewise, onset only')
+			# test_high_low_voice_notewise(notes_system, intervals_system, notes_target, intervals_target, match_on)  # onset only
 
-			print('\n>>>> test high_low_voice notewise, onset and offset')
-			test_high_low_voice_notewise(notes_system, intervals_system, notes_target, intervals_target, match_onoff)  # onset and offset
+			# print('\n>>>> test high_low_voice notewise, onset and offset')
+			# test_high_low_voice_notewise(notes_system, intervals_system, notes_target, intervals_target, match_onoff)  # onset and offset
 
-			print('\n test loudness =============================================')
-			print('\n>>>> test loudness, onset only')
-			test_loudness(match_on, vel_target)
+			# print('\n test loudness =============================================')
+			# print('\n>>>> test loudness, onset only')
+			# test_loudness(match_on, vel_target)
 
-			ratio = loudness_ratio_false_negative(notes_target, intervals_target, vel_target, match_on)
-			print('loudness ratio: ' + str(ratio))
+			# ratio = loudness_ratio_false_negative(notes_target, intervals_target, vel_target, match_on)
+			# print('loudness ratio: ' + str(ratio))
 
-			print('\n test out_key ===============================================')
-			mask = make_key_mask(target_pr)
+			# print('\n test out_key ===============================================')
+			# mask = make_key_mask(target_pr)
 
-			print('\n>>>> non-binary pitch profile, onset only')
-			test_out_key_non_binary(notes_system, match_on, mask)
+			# print('\n>>>> non-binary pitch profile, onset only')
+			# test_out_key_non_binary(notes_system, match_on, mask)
 
-			print('\n>>>> binary pitch profile, onset only')
-			test_out_key_binary(notes_system, match_on, mask)
+			# print('\n>>>> binary pitch profile, onset only')
+			# test_out_key_binary(notes_system, match_on, mask)
 
-			print('\n test polyphony ==============================================')
-			test_polyphony(target_pr, intervals_target, match_on)
+			# print('\n test polyphony ==============================================')
+			# test_polyphony(target_pr, intervals_target, match_on)
 
-			print('\n test repeat_merge ===========================================')
-			test_repeat_merge(notes_system, intervals_system, notes_target, intervals_target, match_on)
+			# print('\n test repeat_merge ===========================================')
+			# test_repeat_merge(notes_system, intervals_system, notes_target, intervals_target, match_on)
 
-			print('\n test specific_pitch ===========================================')
-			test_specific_pitch(system_pr, target_pr, fs)
-			r1, r2 = specific_pitch_notewise(notes_system, intervals_system, notes_target, intervals_target, match_on, n_semitones=1)
-			print(str(r1) + "   " + str(r2))
+			# print('\n test specific_pitch ===========================================')
+			# test_specific_pitch(system_pr, target_pr, fs)
+			# r1, r2 = specific_pitch_notewise(notes_system, intervals_system, notes_target, intervals_target, match_on, n_semitones=1)
+			# print(str(r1) + "   " + str(r2))
 
 			print('\n test rhythm =====================================================')
 			f1, f2 = rhythm_histogram(intervals_system, intervals_target)
-			print("spectral flatness: " + str(f1) + "   " + str(f2))
+			print("logged spectral flatness: " + str(f1) + "(output)   " + str(f2) + "(target)")
+			mean_drift, max_drift = rhythm_dispersion(intervals_system, intervals_target)
+			print("cluster centre drift: " + str(mean_drift) + "(mean)  " + str(max_drift) + "(max)")
 
 
 
